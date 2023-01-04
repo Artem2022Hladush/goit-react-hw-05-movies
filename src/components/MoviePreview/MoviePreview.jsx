98ad93493f1c0f25a9f9c1807aaefab149915434
Артем Гладуш <m1hommage5008@gmail.com>
2023-01-04 16:43:04 +0200

    bag fix moviePreview

--- src/components/MoviePreview/MoviePreview.jsx
+++ src/components/MoviePreview/MoviePreview.jsx
@@ -1,15 +1,15 @@
 import {
-  Container,
-  Genre,
-  GenresList,
-  Image,
-  Info,
-  InfoTitle,
-  MovieTitle,
-  Score,
+	Container,
+	Genre,
+	GenresList,
+	Image,
+	Info,
+	InfoTitle,
+	MovieTitle,
+	Score,
 } from './MoviePreview.styled';
 
-export const MoviePreview = ({ movie}) => {
+export const MoviePreview = ({ movie }) => {
 	const {
 		poster_path,
 		original_title,
@@ -17,29 +17,29 @@ export const MoviePreview = ({ movie}) => {
 		overview,
 		release_date,
 		genres,
-	 } = movie;
+	} = movie;
 
-	return(
+	return (
 		<Container>
-      <Image
-      	src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-      	alt={original_title}
-      />
-      <div>
-      	<MovieTitle>
-         	{original_title} {release_date}
-      	</MovieTitle>
-      	<InfoTitle>User score: </InfoTitle>
-        <Score>{Math.floor(vote_average * 10)} %</Score>
-      	<InfoTitle>Overview</InfoTitle>
-      	{overview && <Info>{overview}</Info>}
-      	<InfoTitle>Genres</InfoTitle>
-      	<GenresList>
-         	{genres.map(({id, name}) => {
-            return <Genre key={id}>{name}</Genre>;
-         	})}
-      	</GenresList>
-      </div>
-   	</Container>
+			<Image
+				src={poster_path}
+				alt={original_title}
+			/>
+			<div>
+				<MovieTitle>
+					{original_title} {release_date}
+				</MovieTitle>
+				<InfoTitle>User score: </InfoTitle>
+				<Score>{Math.floor(vote_average * 10)} %</Score>
+				<InfoTitle>Overview</InfoTitle>
+				{overview && <Info>{overview}</Info>}
+				<InfoTitle>Genres</InfoTitle>
+				{genres && <GenresList>
+					{genres.map(({ id, name }) => {
+						return <Genre key={id}>{name}</Genre>;
+					})}
+				</GenresList>}
+			</div>
+		</Container>
 	)
 }
